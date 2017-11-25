5fe79f6c5a3ea47873fbadc38009fe39e5375ca1
Tom Hutchings <goerofmuns@gmail.com>
2017-11-25 21:58:54 +0000

    proper recursive node print function and some comments

--- src/testparser.cpp
+++ src/testparser.cpp
@@ -44,22 +44,30 @@ std::map<NodeType, std::string> nodeNameMap =
     {NodeType::None, "None"}
 };
 
-void printTree(AST *ast, int indent = 0)
+void printNode(Node node, int indent = 0)
 {
-
-    for (auto node : ast->sections)
+    for(auto arg : node.args)
     {
-	
+	// add indent based on current tree depth
+	std::cout << std::string(indent, ' ') << arg.first << " : " << arg.second << "\n";
+    }
+
+    //if we have children print them
+    if(!node.block.empty())
+    {
+	for(auto child : node.block)
+	    printNode(child, indent + 1);
     }
 }
 
 int main()
 {
+    // read file
     ifstream *f = new ifstream("test.txt");
+
+    // attempt to lex file f into token vector v
     vector<Parlex::Token> v;
-
     Parlex::Lexer l(f);
-
     try
     {
 	v = l.lex();
@@ -72,6 +80,7 @@ int main()
 	return 1;
     }
 
+    //print token vector
     std::cout << "Lexer output:" << std::endl;
     for(auto t : v)
     {
@@ -79,21 +88,29 @@ int main()
     }
 
 
+    //create parser from token vec and parse into AST var
     Parlex::Parser p(v);
     AST* ast;
     ast = p.parse();
 
+    //print AST
     std::cout << std::endl << "Parser output:" << std::endl;
 
     //print setup block real quick
+    std::cout << "Setup Block:\n";
     for (auto node : ast->setup.block)
     {
-	std::cout << "Node type: " << nodeNameMap[node.type] << "\n";
+	std::cout << "Node : " << nodeNameMap[node.type] << "\n";
 	for (const auto &arg : node.args)
 	    std::cout << " - " << arg.first << " : " << arg.second << "\n";
 	std::cout << "\n";
     }
 
-    //print full tree
-    printTree(ast);
+    //print each section
+    std::cout << "Section Blocks:\n";
+    for (auto section : ast->sections)
+    {
+	//recur print child nodes of section
+	printNode(section);
+    }
 }
